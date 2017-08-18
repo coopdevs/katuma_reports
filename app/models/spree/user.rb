2966967ee7c4bdcc976c6a69af0ed90685cad15c
Pau Perez <saulopefa@gmail.com>
2017-08-18 18:10:16 +0200

    Make user model read-only
    We don't want to screw up things in production :trollface:

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -11,5 +11,19 @@ module Spree
     # Setup accessible (or protected) attributes for your model
     attr_accessible :email, :password, :password_confirmation, :remember_me
     # attr_accessible :title, :body
+
+    attr_readonly(*column_names) # Required to block update_attribute and update_column
+
+    def readonly?
+      true
+    end
+
+    def destroy
+      raise ReadOnlyRecord
+    end
+
+    def delete
+      raise ReadOnlyRecord
+    end
   end
 end
