6f9ac03e7133e8dbb85f7af672109c759de6be71
Pau Perez <saulopefa@gmail.com>
2017-08-21 08:26:34 +0200

    Use the same devise modules as spree_auth_devise

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -2,11 +2,8 @@ module Spree
   class User < ActiveRecord::Base
     self.table_name = 'spree_users'
 
-    # Include default devise modules. Others available are:
-    # :token_authenticatable, :confirmable,
-    # :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-      :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :token_authenticatable, :registerable, :recoverable,
+           :rememberable, :trackable, :validatable, :encryptable, encryptor: 'authlogic_sha512'
 
     # Setup accessible (or protected) attributes for your model
     attr_accessible :email, :password, :password_confirmation, :remember_me
