1145097d2f3c08b3913b79a0df9025830ad21c5f
Pau Perez <saulopefa@gmail.com>
2017-08-21 11:29:50 +0200

    Get Devise's redirect_url from env var
    This allows configuring the redirect_url depending on the environment
    and changing it in the future.

--- lib/redirect_to_ofn_failure.rb
+++ lib/redirect_to_ofn_failure.rb
@@ -1,6 +1,13 @@
 class RedirectToOFNFailure < Devise::FailureApp
+
+  # Overrides the URL we redirect to when the request is not authorized. This
+  # will happen, for instance, when the user logs out from the main OFN app.
+  #
+  # You must provide the $OFN_MAIN_APP_BASE_URL env var as follows:
+  #
+  #   export OFN_MAIN_APP_BASE_URL="http://localhost:3000"
   def redirect_url
-    'http://localhost:3000'
+    URI.join(ENV.fetch('OFN_MAIN_APP_BASE_URL'), 'login').to_s
   end
 
   # You need to override respond to eliminate recall
