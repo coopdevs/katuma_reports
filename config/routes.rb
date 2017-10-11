a99a3dd464f3a63bc73495bbbe74a3fdd5fe1b6c
Pau Perez <saulopefa@gmail.com>
2017-10-11 12:00:48 +0200

    Add doc

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ KatumaReports::Application.routes.draw do
     scope '/reports' do
       resources :variants_by_order, only: %i[index]
 
+      # This will match any URL like /admin/reports/<report_name> not matched
+      # by any of the previous declarations. Remember that Rails evaluates the
+      # routes from top to bottom.
       get '/:report', controller: :reports, action: :index
     end
   end
