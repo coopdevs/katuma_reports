5abaefc018e39b1892218032768f5c1014fec063
Pau Perez <saulopefa@gmail.com>
2017-10-17 15:05:14 +0200

    Scope the report URL under the overriden report name
    This way nginx can match and point the request to Katuma reports without
    the need for any new location block.

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ KatumaReports::Application.routes.draw do
 
   scope '/admin' do
     scope '/reports' do
-      resources :variants_by_order, only: %i[index]
+      get '/order_cycle_management', controller: :reports, action: :index
 
-      # This will match any URL like /admin/reports/<report_name> not matched
-      # by any of the previous declarations. Remember that Rails evaluates the
-      # routes from top to bottom.
-      get '/:report', controller: :reports, action: :index
+      scope '/order_cycle_management' do
+        resources :variants_by_order, only: %i[index]
+      end
     end
   end
 end
