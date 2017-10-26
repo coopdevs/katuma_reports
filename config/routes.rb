536ed17ac4473549f156b7cff98955b590429567
Pau Perez <saulopefa@gmail.com>
2017-10-26 18:00:16 +0200

    Fix delayed_job route

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ KatumaReports::Application.routes.draw do
 
   root controller: :reports, action: :index
 
-  match '/delayed_job' => DelayedJobWeb, anchor: false, via: [:get, :post]
-
   scope '/admin' do
     scope '/reports' do
       get '/order_cycle_management', controller: :reports, action: :index
 
       scope '/order_cycle_management' do
         resources :variants_by_order, only: %i[index]
+
+        match '/delayed_job' => DelayedJobWeb, anchor: false, via: [:get, :post]
       end
     end
   end
